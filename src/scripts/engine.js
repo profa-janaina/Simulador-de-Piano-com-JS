7e814aee69359bf1b2811d655116e65799423000
Joao e larissa <dev.joaovictor22@gmail.com>
2023-11-20 09:39:05 -0300

    fix: fixed piano keys behavior.

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -1,42 +1,54 @@
+// Elementos do HTML
 const pianoKeys = document.querySelectorAll(".piano-keys .key");
 const volumeSlider = document.querySelector(".volume-slider input");
 const keysCheck = document.querySelector(".keys-check input")
 
-let mapedKeys = [];
+// Array para as teclas do app
+const mapedKeys = [];
+const audios = {}; 
 
-let audio = new Audio('src/tunes/a.wav')
-
+// FIX : Função que reproduz as notas.
 const playTune = (key) => {
-    audio.src = `src/tunes/${key}.wav`
+    const audio = new Audio(`src/tunes/${key.toLowerCase()}.wav`);
+    audios[key] = audio;
+
+    // Adicionando dinâmica sonora. 
+    audio.addEventListener('ended', () => delete audios[key]);
     audio.play();
 
-    const clickedKey = document.querySelector(`[data-key="${key}"]`);
-  clickedKey.classList.add("active");
-  setTimeout(() => {
-    clickedKey.classList.remove("active");
-  }, 150);
+    // Animação das teclas
+    const clickedKey = document.querySelector(`[data-key="${key.toLowerCase()}"]`);
+    clickedKey.classList.add("active");
+    setTimeout(() => clickedKey.classList.remove("active"), 150);
 };
 
-pianoKeys.forEach((keyParameter)=>{
-    keyParameter.addEventListener('click', () => playTune(keyParameter.dataset.key));
+// Função queas notas
+const stopTune = (key) => (audios[key]) ? delete audios[key] : void(0);
 
+// Adicionando as notas para a array adicionando um listener pra cada uma delas.
+pianoKeys.forEach((keyParameter) => {
+    keyParameter.addEventListener('click', () => playTune(keyParameter.dataset.key));
     mapedKeys.push(keyParameter.dataset.key);
 });
 
+// FIX : Escutando os botões do teclado para as notas.
 document.addEventListener('keydown', (e) => {
-    if(mapedKeys.includes(e.key)){
-        playTune(e.key);
-    }    
-})
+    // Operador ternário para teste lógico.
+    // Operador ternário nada mais é que um if voltada para apenas uma linha de codigo
+    // tanto no true quanto no false, no qual voce consegue colocar dentro de uma variavel se quiser.
+    // Syntax : <condição> ? <operação 1> : <operação 2>;
+    (mapedKeys.includes((e.key.toLowerCase() || e.key.toUpperCase())) && !audios[e.key]) ? playTune(e.key) : void(0);
+});
 
-const handleVolume = (e) => {
-audio.volume = e.target.value;
-};
+// FIX : Restaurando o trigger para os botões do teclado.
+document.addEventListener('keyup', (e) => stopTune(e.key));
 
-const showHideKeys = () => {
-    pianoKeys.forEach((key) => key.classList.toggle("hide"));
-}
+// Som
+const handleVolume = (e) => {audio.volume = e.target.value};
 
+// Escondendo teclas.
+const showHideKeys = () => {pianoKeys.forEach((key) => key.classList.toggle("hide"))};
+
+// Listeners do volume e das keys.
 volumeSlider.addEventListener("input", handleVolume);
-
 keysCheck.addEventListener('click', showHideKeys);
